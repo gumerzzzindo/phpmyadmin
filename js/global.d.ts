1bcabc4d0deed79efa089264a8cd480d42bccf6d
gumerzzzindo <gumerzzzindo@users.noreply.github.com>
2022-09-28 00:30:15 +0200

    Update global.d.ts

--- js/global.d.ts
+++ js/global.d.ts
@@ -1,5 +1,6 @@
 // libraries/classes/Controllers/JavaScriptMessagesController.php
 declare var Messages: { [p: string]: string };
+declare var messages: { [p: string]: string };
 
 // templates/javascript/variables.twig
 declare var firstDayOfCalendar: string;
